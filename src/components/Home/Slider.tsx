7e0706e27de02474885d940b0bccc58260b4fd0e
Agustin <damian.irala@outlook.com>
2024-09-21 13:44:23 -0300

    Basic CardInfo Structure

--- src/components/Home/Slider.tsx
+++ src/components/Home/Slider.tsx
@@ -3,11 +3,17 @@ import CardInfo from "./Slider/CardInfo"
 export default function Slider() {
   return (
     <>
-      <section className="w-full h-50% bg-white relative flex  gap-10 ">
+      <section className="w-full h-full bg-white relative flex-col items-center justify-center flex gap-3">
+        <header className="text-color-black bg-black flex flex-col">
+            <h1 className="">Nuestros Escultores</h1>
+            <p className="p-0 m-0">Nuestros mejores escultores</p>
+        </header>
+        <div className="flex gap-10 justify-center items-center" >
+          <CardInfo  />
           <CardInfo />
           <CardInfo />
           <CardInfo />
-          <CardInfo />
+        </div>
       </section>
     </>
   )
--- src/components/Home/Slider/CardInfo.tsx
+++ src/components/Home/Slider/CardInfo.tsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import Image from 'next/image'
+import esc from './CardInfo/Venus_of_Arles_Louvre_Ma439_n08.webp'
 
-export default function CardInfo() {
+interface CardInfoProps {
+  pais: string;
+  años: number;
+  nombre: string;
+  img: string;
+}
+
+export default function CardInfo({pais, años,nombre,img}: CardInfoProps) {
   return (
-    <div className='flex w-16 h-20 bg-blue-300'>
-        <div>
-
+    <div className='flex w-32 h-auto bg-blue-300 z-3 flex-col items-center relative'>
+        <header className='relative'>
+            <h1 className='relative text-xl '>Nombre</h1>
+        </header>
+        <div className='relative w-full h-full'>
+            <Image alt="Descripción de la imagen" src={esc} className='w-full h-full'></Image>
         </div>
-
-
+        <div className='relative h-auto w-full bg-black flex flex-col items-center justify-between'>
+            <div className='relative h-auto bg-slate-400 w-full flex flex-col justify-center'>
+                <div className='relative flex justify-center '>
+                    <h2>Detalles</h2>
+                </div>
+                <p>Pais: <span>{pais}</span></p>
+                <p>Años: <span>{años}</span></p>
+            </div>
+            <button className='relative bg-slate-500'>Votar</button>
+        </div>
     </div>
   )
 }
