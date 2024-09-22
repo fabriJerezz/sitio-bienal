6f3c4120224322c503f1a1cbad8ceac0ba4aa8b3
Agustin <damian.irala@outlook.com>
2024-09-22 12:25:52 -0300

    Basic Structure Hidden Info

--- src/components/Home/SculptureShow/CardInfo.tsx
+++ src/components/Home/SculptureShow/CardInfo.tsx
@@ -1,28 +1,39 @@
 "use client";
+import React from 'react'
+import { useState } from 'react';
 import Image from 'next/image'
 import esc from './public/Venus_of_Arles_Louvre_Ma439_n08.webp'
 import HiddenInfo from './CardInfo/HiddenInfo';
 
 
+
 interface CardInfoProps {
   pais: string;
-  años: number;
   nombre: string;
   img: string;
-  art: string;
-  tec: string;
+  
 }
 
 export default function CardInfo({pais,nombre,img}: CardInfoProps) {
+    const [showHidden, setShowHidden] = useState(false);
+
+    const handleClick = () => {
+        setShowHidden(!showHidden);
+    }
   return (
-     <div className='flex w-80 bg-primary z-3 flex-col items-center relative rounded-lg border-secondary border p-2 transform transition-transform hover:scale-105 cursor-pointer'>
-        <header className='relative'>
-            <h1 className="relative text-xl tracking-widest text-secondary font-bold m-2">Nombre</h1>
-        </header>
-        <div className='relative w-full h-full'>
-            <Image alt="Descripción de la imagen" src={esc} className='w-full h-full'></Image>
+    <>
+        <div onClick={handleClick} className='flex w-80 bg-primary z-3 flex-col relative rounded-lg border-secondary border p-2 transform transition-transform hover:scale-105 cursor-pointer'>
+            <header className='relative  mb-2'>
+                <h1 className="relative text-3xl tracking-widest text-secondary font-bold">Nombre</h1>
+                <h2 className='relative text-xl ml-3 italic'>Buenos Aires</h2>
+            </header>
+            <div className='relative w-full h-full'>
+                <Image alt="Descripción de la imagen" src={esc} className='w-full h-full'></Image>
+            </div>
+            <button className='mt-5 relative bg-secundary border-secondary border pr-10 pl-10 pt-1 pb-1 rounded-lg text-xl  '>Votar</button>
         </div>
-        <button className='mt-5 relative bg-secundary border-secondary border pr-10 pl-10 pt-1 pb-1 rounded-lg text-xl  '>Votar</button>
-    </div>
+       {showHidden && <HiddenInfo handleClick={handleClick} />}
+    </>
+    
   )
 }
--- src/components/Home/SculptureShow/CardInfo/HiddenInfo.tsx
+++ src/components/Home/SculptureShow/CardInfo/HiddenInfo.tsx
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
-function HiddenInfo() {
-  return (
-    <div>
-        <div className='relative flex justify-center border-b p-0'>
-            <h2 className='p-0 m-0 text-xl'>Detalles</h2>
-        </div>
-        <ul className='text-sm flex flex-col m-1  w-full gap-1'>
-            <li >Pais: <span>Francia</span></li>
-            <li >Años: <span>1000</span></li>
-            <li >Movimiento Artistico: <span>Renacimiento</span></li>
-            <li >Tecnica: <span>Escultura en marmol</span></li>
-        </ul>
-    </div>
-  )
+interface HiddenInfoProps {
+    handleClick: () => void;
+}
+
+const HiddenInfo: React.FC<HiddenInfoProps> = ({ handleClick }) => {
+    const ref = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        function checkClick(e: MouseEvent) {
+            if (ref.current && !ref.current.contains(e.target as Node)) {
+                handleClick();
+            }
+        }
+
+        document.addEventListener("mousedown", checkClick);
+        return () => {
+            document.removeEventListener("mousedown", checkClick);
+        };
+    }, []);
+
+    return ( 
+            <div ref={ref} className='bg-red-500 w-screen h-screen absolute z-50 top-0 left-0 bottom-0 right-0 flex justify-center items-center'>
+                    <div className='bg-green-500  relative w-1/2 h-1/2 flex flex-col items-center justify-center border-b p-0'>
+
+                            <div>
+                                <ul className='text-sm flex flex-col m-1  w-full gap-1'>
+                                        <li >Pais: <span>Francia</span></li>
+                                        <li >Años: <span>1000</span></li>
+                                        <li >Movimiento Artistico: <span>Renacimiento</span></li>
+                                        <li >Tecnica: <span>Escultura en marmol</span></li>
+                                </ul>
+                            </div>
+                    </div>
+            </div>
+    )
 }
 
 export default HiddenInfo
