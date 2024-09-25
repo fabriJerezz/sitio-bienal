6a265b52a314d3f0f5056c410a282303736b3e56
Agustin <damian.irala@outlook.com>
2024-09-25 11:40:31 -0300

    Fetching Data Functional

--- src/components/Home/carousel-size.tsx
+++ src/components/Home/carousel-size.tsx
@@ -13,15 +13,40 @@ import { useEffect, useState } from 'react';
 import CardInfo from './SculptureShow/CardInfo';
 
 export function CarouselSize() {
-  const [isMounted, setIsMounted] = useState(false);
+  const [sculptors, setSculptors] = useState([]);
+
+  const fetchSculptors = async () => {
+    try {
+      console.log('Fetching sculptors...');
+      const response = await fetch(
+        'https://tp-final-bienal.onrender.com/api/escultores/',
+        {
+          headers: {
+            Accept: 'application/json',
+          },
+        }
+      );
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      console.log('Fetched data:', data);
+      return data;
+    } catch (error) {
+      console.error('Failed to fetch sculptors:', error);
+      return [];
+    }
+  };
 
   useEffect(() => {
-    setIsMounted(true);
+    const getSculptors = async () => {
+      console.log('Calling fetchSculptors...');
+      const fetchedSculptors = await fetchSculptors();
+      console.log('Setting sculptors state...');
+      setSculptors(fetchedSculptors);
+    };
+    getSculptors();
   }, []);
-
-  if (!isMounted) {
-    return null;
-  }
 
   return (
     <div className="w-full max-w-5xl mx-auto px-4">
@@ -33,11 +58,20 @@ export function CarouselSize() {
         className="w-full"
       >
         <CarouselContent className="-ml-2 md:-ml-4">
-          {Array.from({ length: 5 }).map((_, index) => (
+          {sculptors.map((sculptor, index) => (
             <CarouselItem key={index} className="pl-2 md:pl-4 md:basis-1/3">
               <Card className="border border-gray-200">
                 <CardContent className="flex flex-col items-center justify-center p-6">
-                  <CardInfo />
+                  <CardInfo
+                    key={index}
+                    country={sculptor.pais}
+                    name={sculptor.nombre}
+                    img={sculptor.imagen}
+                    mov={sculptor.movimiento}
+                    history={sculptor.historia}
+                    province={sculptor.provincia}
+                    technique={sculptor.tecnica}
+                  />
                 </CardContent>
               </Card>
             </CarouselItem>
