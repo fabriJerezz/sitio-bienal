1fbefa17598d8cb37a09bd0d254a8b8a2f9e15b3
Agustin <damian.irala@outlook.com>
2024-09-26 15:46:29 -0300

    Gallery Modification

--- src/components/Home/galleryworks/Gallery.tsx
+++ src/components/Home/galleryworks/Gallery.tsx
@@ -9,9 +9,9 @@ import img6 from '../../../../public/imgs/esc6.jpg';
 
 const Gallery = () => {
   return (
-    <div className="relative w-screen h-screen flex flex-col bg-black ">
-      <div className="inline-flex flex-col p-1 border-white border-right mt-5 mb-5 ml-5">
-        <h1 className="text-4xl font-bold text-white border-r-4 border-white  w-max pr-5">
+    <div className="relative w-screen h-screen flex flex-col bg-black overflow-hidden">
+      <div className="inline-flex flex-col p-1 border-white border-right mt-5  ml-5">
+        <h1 className="text-4xl font-bold text-white border-r-4 border-white w-max pr-5">
           Galeria de
         </h1>
         <h2 className="text-4xl font-extrabold text-white ml-40 mt-3 w-max border-r-4 border-white pr-5">
@@ -19,74 +19,116 @@ const Gallery = () => {
         </h2>
       </div>
       <div className="relative w-screen h-screen overflow-hidden">
-        <div className="relative w-full h-full animate-scroll">
-          <Image
-            src={img1}
-            alt="obra"
-            className="absolute w-90 h-auto"
-            style={{ top: '10%', left: '30%' }}
-          />
-          <Image
-            src={img2}
-            alt="obra"
-            className="absolute w-90 h-auto"
-            style={{ bottom: '30%', right: '40%' }}
-          />
-          <Image
-            src={img3}
-            alt="obra"
-            className="absolute w-90 h-auto"
-            style={{ top: '50%', left: '30%' }}
-          />
-          <Image
-            src={img4}
-            alt="obra"
-            className="absolute w-90 h-auto"
-            style={{ top: '70%', left: '80%' }}
-          />
-          <Image
-            src={img5}
-            alt="obra"
-            className="absolute w-90 h-auto"
-            style={{ top: '20%', right: '70%' }}
-          />
-          <Image
-            src={img6}
-            alt="obra"
-            className="absolute w-90 h-auto"
-            style={{ top: '60%', left: '40%' }}
-          />
-          <Image
-            src={img6}
-            alt="obra"
-            className="absolute w-90 h-auto"
-            style={{ top: '20%', left: '30%' }}
-          />
-          <Image
-            src={img6}
-            alt="obra"
-            className="absolute w-90 h-auto"
-            style={{ top: '30%', left: '70%' }}
-          />
-          <Image
-            src={img6}
-            alt="obra"
-            className="absolute w-90 h-auto"
-            style={{ top: '60%', left: '70%' }}
-          />
+        <div className="absolute w-[300%] h-full animate-scroll">
+          <div className="relative w-1/3 h-full float-left">
+            <Image
+              src={img1}
+              alt="obra"
+              className="absolute w-1/4 h-auto top-[2%] left-[10%]"
+            />
+            <Image
+              src={img2}
+              alt="obra"
+              className="absolute w-1/4 h-auto top-[15%] left-[40%]"
+            />
+            <Image
+              src={img3}
+              alt="obra"
+              className="absolute w-1/4 h-auto top-[30%] left-[20%]"
+            />
+            <Image
+              src={img4}
+              alt="obra"
+              className="absolute w-1/4 h-auto top-[15%] left-[70%]"
+            />
+            <Image
+              src={img5}
+              alt="obra"
+              className="absolute w-1/4 h-auto top-[20%] left-[60%]"
+            />
+            <Image
+              src={img6}
+              alt="obra"
+              className="absolute w-1/4 h-auto top-[50%] left-[80%]"
+            />
+          </div>
+          <div className="relative w-1/3 h-full float-left">
+            <Image
+              src={img1}
+              alt="obra"
+              className="absolute w-1/4 h-auto top-[10%] left-[10%]"
+            />
+            <Image
+              src={img2}
+              alt="obra"
+              className="absolute w-1/4 h-auto top-[30%] left-[40%]"
+            />
+            <Image
+              src={img3}
+              alt="obra"
+              className="absolute w-1/4 h-auto top-[60%] left-[20%]"
+            />
+            <Image
+              src={img4}
+              alt="obra"
+              className="absolute w-1/4 h-auto top-[15%] left-[70%]"
+            />
+            <Image
+              src={img5}
+              alt="obra"
+              className="absolute w-1/4 h-auto top-[50%] left-[60%]"
+            />
+            <Image
+              src={img6}
+              alt="obra"
+              className="absolute w-1/4 h-auto top-[45%] left-[80%]"
+            />
+          </div>
+          <div className="relative w-1/3 h-full float-left">
+            <Image
+              src={img1}
+              alt="obra"
+              className="absolute w-1/4 h-auto top-[10%] left-[10%]"
+            />
+            <Image
+              src={img2}
+              alt="obra"
+              className="absolute w-1/4 h-auto top-[35%] left-[40%]"
+            />
+            <Image
+              src={img3}
+              alt="obra"
+              className="absolute w-1/4 h-auto top-[30%] left-[20%]"
+            />
+            <Image
+              src={img4}
+              alt="obra"
+              className="absolute w-1/4 h-auto top-[15%] left-[70%]"
+            />
+            <Image
+              src={img5}
+              alt="obra"
+              className="absolute w-1/4 h-auto top-[20%] left-[60%]"
+            />
+            <Image
+              src={img6}
+              alt="obra"
+              className="absolute w-1/4 h-auto top-[50%] left-[80%]"
+            />
+          </div>
         </div>
       </div>
       <style jsx>{`
         @keyframes scroll {
           0% {
-            transform: translateY(0);
+            transform: translateX(0);
           }
           100% {
-            transform: translateY(-100%);
+            transform: translateX(-66.666%);
           }
         }
         .animate-scroll {
-          animation: scroll 20s linear infinite;
+          animation: scroll 52.5s linear infinite;
         }
       `}</style>
     </div>
