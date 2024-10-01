e788161e46c6901b9123e8508892abe156971fa7
Agustin <damian.irala@outlook.com>
2024-09-30 23:58:10 -0300

    User Path Add

---
+++ src/app/admin/usuarios/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import UsersList from '@/components/Admin/Usuarios/UsersList';
+
+function page() {
+  return (
+    <div>
+      <h1>Usuarios</h1>
+      <UsersList />
+    </div>
+  );
+}
+
+export default page;
