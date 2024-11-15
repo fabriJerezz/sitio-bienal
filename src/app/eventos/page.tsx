9ca1e71c7bde878220c01d339d8faffb0ef84493
Irala note <damian.irala@outlook.com>
2024-11-15 15:35:49 -0300

    Enhancenment Events View

--- src/app/eventos/page.tsx
+++ src/app/eventos/page.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
 import EventsCard from '../../components/Events/EventsCard';
-import { useEffect, useState } from 'react';
 import { Event } from '@/types';
 
 export default function Events() {
-  const [events, setEvents] = useState([]);
+  const [events, setEvents] = useState<Event[]>([]);
 
   useEffect(() => {
-    fetch('http://localhost:3001/events')
-      .then((res) => res.json())
-      .then((data) => setEvents(data));
+    const fetchEvents = async () => {
+      let allEvents: Event[] = [];
+      let url = 'https://tp-final-bienal.onrender.com/api/eventos/';
+
+      while (url) {
+        const response = await fetch(url);
+        const data = await response.json();
+        allEvents = [...allEvents, ...data.results];
+        url = data.next; // Actualiza la URL para la siguiente página
+      }
+
+      setEvents(allEvents);
+    };
+
+    fetchEvents();
   }, []);
-  return <></>;
+
+  return (
+    <div>
+      <EventsCard events={events} />
+    </div>
+  );
 }
--- src/components/Events/EventsCard.tsx
+++ src/components/Events/EventsCard.tsx
@@ -15,51 +15,49 @@ interface CardEventsProps {
 
 export default function EventsCard({ events }: CardEventsProps) {
   return (
-    <>
-      <div className="flex relative w-full h-screen bg-gradient-to-r from-gray-900 via-gray-700 to-black">
-        <div className="flex flex-col relative w-2/5 h-full bg-slate-400 items-center justify-center">
-          <h1 className="items-center justify-center flex w-full text-white font-extrabold text-4xl ">
-            <div className="tracking-wide flex flex-col w-fit border-b-4 pb-1">
-              Proximos <span>Eventos</span>
-            </div>
-          </h1>
-          <p className="text-white text-center mt-3">
-            Eventos que se realizaran
-            <br />
-            en las proximas fechas
-          </p>
-          <div className="flex">
-            <CarouselNext />
-            <CarouselPrevious />
+    <div className="flex relative w-full h-screen bg-gradient-to-r from-gray-900 via-gray-700 to-black">
+      <div className="flex flex-col relative w-2/5 h-full  items-center justify-center">
+        <h1 className="items-center justify-center flex w-full text-white font-extrabold text-4xl ">
+          <div className="tracking-wide flex flex-col w-fit border-b-4 pb-1">
+            Proximos <span>Eventos</span>
           </div>
-        </div>
-        <div className="zonaSlider w-3/5 h-full bg-slate-50">
-          <Carousel
-            opts={{
-              align: 'start',
-              loop: true,
-            }}
-          >
-            <CarouselContent>
-              {events.map((event) => (
-                <CarouselItem
-                  key={event.id}
-                  className="md:basis-1/2 lg:basis-1/3"
-                >
-                  <div className="flex flex-col w-full h-full items-center justify-center">
-                    <h1 className="text-4xl font-extrabold text-slate-900">
-                      {event.nombre}
-                    </h1>
-                    <p className="text-slate-900">{event.fecha_inicio}</p>
-                    <p className="text-slate-900">{event.fecha_final}</p>
-                    <p className="text-slate-900">{event.descripcion}</p>
-                  </div>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-          </Carousel>
-        </div>
+        </h1>
+        <p className="text-white text-center mt-3">
+          Eventos que se realizaran
+          <br />
+          en las proximas fechas
+        </p>
       </div>
-    </>
+      <div className="zonaSlider w-3/5 h-full  flex justify-center items-center ">
+        <Carousel
+          opts={{
+            align: 'start',
+            loop: true,
+          }}
+        >
+          <CarouselContent>
+            {events.map((event) => (
+              <CarouselItem
+                key={event.id}
+                className="md:basis-1/2 lg:basis-1/3"
+              >
+                <div className="flex flex-col w-full h-full items-center justify-center p-4 bg-white shadow-lg rounded-lg">
+                  <h1 className="text-4xl font-extrabold text-slate-900 mb-2">
+                    {event.nombre}
+                  </h1>
+                  <p className="text-slate-900 mb-1">{event.fecha_inicio}</p>
+                  <p className="text-slate-900 mb-1">{event.fecha_final}</p>
+                  <p className="text-slate-900">{event.descripcion}</p>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <div className="flex justify-between mt-4">
+            <CarouselPrevious />
+            <CarouselNext />
+          </div>
+        </Carousel>
+      </div>
+    </div>
   );
 }
