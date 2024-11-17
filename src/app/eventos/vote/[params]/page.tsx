0d70c3df68445229ebc6f8cf724f0814017e381e
Irala note <damian.irala@outlook.com>
2024-11-17 20:49:41 -0300

    Instant Vote When u scan QRCODE

--- src/app/eventos/vote/[params]/page.tsx
+++ src/app/eventos/vote/[params]/page.tsx
@@ -8,57 +8,54 @@ import Link from 'next/link';
 const Page = () => {
   const { user } = useUserStore();
   const { params } = useParams();
-  const [token, setToken] = useState('');
-  const [rating, setRating] = useState('');
-  const [pieceId, setPieceId] = useState('');
 
   console.log('User', user);
-
-  const postVote = async () => {
-    const url = `https://tp-final-bienal.onrender.com/vote/${pieceId}/${token}/`;
-    console.log('URL:', url);
-    console.log('Token:', user?.token);
-    console.log('Rating:', rating);
-
-    try {
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Token ${user?.token}`,
-        },
-        body: JSON.stringify({
-          puntuacion: rating,
-        }),
-      });
-
-      if (response.ok) {
-        alert('Voto Registrado');
-      } else {
-        console.error('Error en la respuesta:', response.statusText);
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
 
   useEffect(() => {
     if (params && typeof params === 'string') {
       const [someToken, somePieceId, someRating] = params.split('-');
-      setToken(someToken);
-      setPieceId(somePieceId);
-      setRating(someRating);
-    }
-  }, [params]);
+      console.log('Token:', someToken);
+      console.log('PieceId:', somePieceId);
+      console.log('Rating:', someRating);
 
-  console.log('Token', token);
-  console.log('PieceId', pieceId);
-  console.log('Rating', rating);
+      console.log(user);
+
+      const postVote = async () => {
+        const url = `https://tp-final-bienal.onrender.com/vote/${somePieceId}/${someToken}/`;
+        console.log('URL:', url);
+        console.log('Token:', user?.token);
+        console.log('Rating:', someRating);
+
+        try {
+          const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Token ${user?.token}`,
+            },
+            body: JSON.stringify({
+              puntuacion: someRating,
+            }),
+          });
+
+          if (response.ok) {
+            alert('Voto Registrado');
+          } else {
+            const errorData = await response.json();
+            console.error('Error en la respuesta:', errorData.detail);
+          }
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      };
+
+      postVote();
+    }
+  }, []);
 
   return (
     <div className="h-screen w-screen flex justify-center items-center">
       <h1>Vote Page</h1>
-      <button onClick={postVote}>Confirm</button>
       <Link href="/eventos/2">Go back</Link>
     </div>
   );
