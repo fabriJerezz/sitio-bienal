416841ac828879048f7fbe70390f833bd81be52f
Agustin <damian.irala@outlook.com>
2024-12-04 22:04:11 -0300

    Stable Gallery

--- src/components/Home/galleryworks/Gallery.tsx
+++ src/components/Home/galleryworks/Gallery.tsx
@@ -1,22 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
-import { useState, useEffect } from 'react';
 import { Piece } from '@/types';
-import img1 from '../../../../public/imgs/esc1.jpg';
-import img2 from '../../../../public/imgs/esc2.jpg';
-import img3 from '../../../../public/imgs/esc3.jpg';
-import img4 from '../../../../public/imgs/esc4.jpg';
-import img5 from '../../../../public/imgs/esc5.jpg';
-import img6 from '../../../../public/imgs/esc6.jpg';
 
 const Gallery = () => {
   const [pieces, setPieces] = useState<Piece[]>([]);
 
   const fetchObras = async (url: string) => {
-    const res = await fetch(url);
-    const data = await res.json();
-
     try {
+      const res = await fetch(url);
+      const data = await res.json();
       setPieces(data.results);
       console.log(data.results);
     } catch (error) {
@@ -28,9 +20,34 @@ const Gallery = () => {
     fetchObras('https://tp-final-bienal.onrender.com/api/obras');
   }, []);
 
+  const renderImages = (pieces: Piece[]) => {
+    const positions = [
+      { top: '2%', left: '10%' },
+      { top: '15%', left: '40%' },
+      { top: '30%', left: '20%' },
+      { top: '15%', left: '70%' },
+      { top: '20%', left: '60%' },
+      { top: '50%', left: '80%' },
+    ];
+
+    return pieces
+      .slice(0, 6)
+      .map((piece, index) => (
+        <Image
+          key={piece.id}
+          src={`https://res.cloudinary.com/dq1vfo4c8/${piece.foto1}`}
+          alt={piece.titulo}
+          width={150}
+          height={150}
+          className="absolute w-1/4 h-auto"
+          style={positions[index]}
+        />
+      ));
+  };
+
   return (
     <div className="relative w-screen h-screen flex flex-col bg-black overflow-hidden">
-      <div className="inline-flex flex-col p-1 border-white border-right mt-5  ml-5">
+      <div className="inline-flex flex-col p-1 border-white border-right mt-5 ml-5">
         <h1 className="text-4xl font-bold text-white border-r-4 border-white w-max pr-5">
           Galeria de
         </h1>
@@ -41,100 +58,13 @@ const Gallery = () => {
       <div className="relative w-screen h-screen overflow-hidden">
         <div className="absolute w-[300%] h-full animate-scroll">
           <div className="relative w-1/3 h-full float-left">
-            <Image
-              src={img1}
-              alt="obra"
-              className="absolute w-1/4 h-auto top-[2%] left-[10%]"
-            />
-            <Image
-              src={img2}
-              alt="obra"
-              className="absolute w-1/4 h-auto top-[15%] left-[40%]"
-            />
-            <Image
-              src={img3}
-              alt="obra"
-              className="absolute w-1/4 h-auto top-[30%] left-[20%]"
-            />
-            <Image
-              src={img4}
-              alt="obra"
-              className="absolute w-1/4 h-auto top-[15%] left-[70%]"
-            />
-            <Image
-              src={img5}
-              alt="obra"
-              className="absolute w-1/4 h-auto top-[20%] left-[60%]"
-            />
-            <Image
-              src={img6}
-              alt="obra"
-              className="absolute w-1/4 h-auto top-[50%] left-[80%]"
-            />
+            {renderImages(pieces)}
           </div>
           <div className="relative w-1/3 h-full float-left">
-            <Image
-              src={img1}
-              alt="obra"
-              className="absolute w-1/4 h-auto top-[10%] left-[10%]"
-            />
-            <Image
-              src={img2}
-              alt="obra"
-              className="absolute w-1/4 h-auto top-[30%] left-[40%]"
-            />
-            <Image
-              src={img3}
-              alt="obra"
-              className="absolute w-1/4 h-auto top-[60%] left-[20%]"
-            />
-            <Image
-              src={img4}
-              alt="obra"
-              className="absolute w-1/4 h-auto top-[15%] left-[70%]"
-            />
-            <Image
-              src={img5}
-              alt="obra"
-              className="absolute w-1/4 h-auto top-[50%] left-[60%]"
-            />
-            <Image
-              src={img6}
-              alt="obra"
-              className="absolute w-1/4 h-auto top-[45%] left-[80%]"
-            />
+            {renderImages(pieces)}
           </div>
           <div className="relative w-1/3 h-full float-left">
-            <Image
-              src={img1}
-              alt="obra"
-              className="absolute w-1/4 h-auto top-[10%] left-[10%]"
-            />
-            <Image
-              src={img2}
-              alt="obra"
-              className="absolute w-1/4 h-auto top-[35%] left-[40%]"
-            />
-            <Image
-              src={img3}
-              alt="obra"
-              className="absolute w-1/4 h-auto top-[30%] left-[20%]"
-            />
-            <Image
-              src={img4}
-              alt="obra"
-              className="absolute w-1/4 h-auto top-[15%] left-[70%]"
-            />
-            <Image
-              src={img5}
-              alt="obra"
-              className="absolute w-1/4 h-auto top-[20%] left-[60%]"
-            />
-            <Image
-              src={img6}
-              alt="obra"
-              className="absolute w-1/4 h-auto top-[50%] left-[80%]"
-            />
+            {renderImages(pieces)}
           </div>
         </div>
       </div>
